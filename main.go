5d83a687fd789604a02ccf77c6d434c714a75cdf
Anita Chang <huilong494@gmail.com>
2021-11-24 21:13:33 -0800

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"wiskey/cmd"
+	"wiskey/http"
+	. "wiskey/pkg"
+)
+
+func main() {
+	parse, err := cmd.Parse()
+	if err != nil {
+		panic(err)
+	}
+	vlog := NewVlog(parse.Vlog, parse.Checkpoint)
+	memtable := NewMemTable(parse.MemtableSize)
+	tree := NewLsmTree(vlog, parse.SStablePath, memtable, 120)
+	http.Start(tree)
+}
